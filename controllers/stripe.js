43619db4bc2a6812f6ca1b4af84e4ec38b01ce14
kishank11 <kishankanojia11@gmail.com>
2022-08-03 09:39:41 +0530

    stripe updated

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -2,25 +2,8 @@ const stripe = require("stripe")(
   "sk_test_51LLRSqSAnHbo4JMTucK0Lc00c5Ncixy3OhH5pv2xZfCFGertTm5rAPFYvUa64116TKqSWvKDv6fNrG2noA4zHCey00k3oF47Nl"
 );
 
-const createSource = async (req, res) => {
-  // const source = stripe.sources.create(
-  //   {
-  //     currency: "USD",
-  //     owner: {
-  //       email: "kishanlal@siti-solutions.com",
-  //     },
-  //   },
-  //   function (err, source) {
-  //     if (err) {
-  //       console.log(err);
-  //       res.json(err);
-  //     }
-  //     // asynchronously called
-  //     console.log(source);
-  //     res.json(source);
-  //   }
-  // );
-  const paymentIntent = await stripe.paymentIntents
+const payment = async (req, res) => {
+  await stripe.paymentIntents
     .create({
       amount: req.body.amount,
       currency: "usd",
@@ -28,24 +11,10 @@ const createSource = async (req, res) => {
     })
     .then((data) => {
       res.json(data);
+    })
+    .catch((e) => {
+      res.json(e);
     });
 };
-const payment = async (req, res) => {
-  stripe.charges.create(
-    {
-      source: "default_source",
-      amount: req.body.amount,
-      currency: "USD",
-      payment_method: "pm_card_visa",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
-};
 
-module.exports = { payment, createSource };
+module.exports = { payment };
--- routes/stripe.js
+++ routes/stripe.js
@@ -3,6 +3,5 @@ const { payment, createSource } = require("../controllers/stripe");
 const router = require("express").Router();
 
 router.post("/payment", payment);
-router.post("/createSource", createSource);
 
 module.exports = router;
